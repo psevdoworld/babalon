c63ba6cf3c5cd54ca210959a62f0b5e14edbdc72
psevdoworld <psevdoworld@yandex.ru>
2022-04-02 15:34:12 +0300

    я сделялт

--- dj/blob/views.py
+++ dj/blob/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from blob.models import Post # <==
+
+from blob.forms import CommentsForm
+from blob.models import Post, Comments # <==
 
 # мой проект/blob/views.py
+
+
+
 def post_list(request):
     posts = Post.objects.all()# <==
     return render(request,
@@ -9,8 +14,20 @@ def post_list(request):
                   {'posteki':posts})# <==
 
 def post_detail(request, pk):
-    post = Post.objects.get(id=pk)
+    this_post = Post.objects.get(id=pk)
+    comments = Comments.objects.filter(post=this_post).all()
+    if request.method == 'GET':
+        form = CommentsForm()
+    elif request.method == 'POST':
+        form = CommentsForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = this_post
+            comment.save()
+
     page = render(request,
                   'blob/post_one.html',
-                  {'post':post})
+                  {'post':this_post,
+                   'comments':comments,
+                   'form':form})
     return page
